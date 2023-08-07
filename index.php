45073b61b0958e14968557025c37945d270b0f22
ItzNoneHxD <96836422+ItzNoneHxD@users.noreply.github.com>
2023-08-07 20:06:40 +0700

    Create index.php
    wdw

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hacked By Akashv</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="Akashv Wuz Here!">
+  <link rel="icon" href="https://cdn.discordapp.com/attachments/1125816362497998909/1126795439761522748/madara-pfp-8.png">
+  <meta name="author" content="oalah">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    body {
+      font-family: "Arial", sans-serif;
+      background-color: black;
+    }
+    h1 {
+      font-size: 36px;
+      font-family: "Inconsolata", monospace;
+      margin-top: 0;
+      color: white;
+    }
+    h2 {
+      font-size: 20px;
+      margin-top: 0;
+      color: white;
+    }
+    h3 {
+      font-size: 30px;
+      color: white;
+    }
+    p {
+      font-size: 17px;
+      color: green;
+    }
+    pler {
+      font-size 2px;
+      color: white;
+      font-family: Share Tech Mono;
+    }
+    .red-text {
+      color: red;
+    }
+    .white-text {
+      color: white;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1><center>root@akashv:~$ <span class="red-text">./wh00pz?!</span> 💔</center></h1>
+    <center>
+    <img src="https://cdn.discordapp.com/attachments/1131889925692866620/1138070129628950648/madara.gif" alt="Madara GIF">
+    </center>
+
+    <center><h3>Hacked By <span class="red-text">Akashv</center></h3>
+    <br><center><font size="2"family="Share Tech Mono" color="grey">akashvsec1@proton.me</center><br>
+    <h2><center>Let me show you what the <span class="red-text">pain <span class="white-text">is</center></h2>
+<center>
+    <p>Greetz to My Niggas Homies</p>
+  </div>
+</body>
+</html>
