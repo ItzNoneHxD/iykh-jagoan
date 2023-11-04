e024320992eadab1ca32e7189876a87928e148ab
ItzNoneHxD <96836422+ItzNoneHxD@users.noreply.github.com>
2023-11-04 18:14:56 +0700

    Create new.php

---
+++ new.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hacked By Akashv</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="Hacked By Akashv">
+  <meta name="keywords" content="Hacked by Akashv">
+  <meta name="author" content="Akashv">
+
+  <script>alert("Hacked by Akashv")</script>
+
+<style>
+  body {
+    background-color: black;
+    color: white;
+    font-size: 24px;
+    font-family: Inconsolata;
+    text-align: center;
+  }
+  h1 {
+    color:red;
+    font-size: 37px;
+    font-family: cursive;
+  }
+  p {
+    font-size:small;
+
+  }
+</style>
+</head>
+<body>
+<i><b>
+<font size="4" face="Inconsolata" color="white">root@<font size="4" face="Inconsolata" color="red">Akashv<font size="4" face="Inconsolata" color="white">:~# ./wh00pz?!? <br></font></font></font></b></i>
+
+
+<img src="https://64.media.tumblr.com/f671d1eb99df5e7c03d8d557bfd8c76c/tumblr_ouaqflULyC1w0ii2ho1_500.gifv" alt="GIF">
+
+<h1>Hacked By Akashv</h1>
+<b>
+<center><font size="4" face="Inconsolata" color="white">Sometimes the people who try to make everyone happy, are the loneliest people</font></center>
+<h3><audio controls autoplay loop>
+  <source src="https://cdn.discordapp.com/attachments/1154977147065217027/1170307530849779782/ssstik.io_1698835154285.mp3?ex=655890d2&is=65461bd2&hm=242df7e351cadabe20a2837c2a0606417780b0a8110f4db1eeb24b9a95beb603" type="audio/mpeg"></audio></h3>
+
+<font face="Courier New" color="white" size="4">Contact Us </b><font color="red">akashvsec1@proton.me</font>
+<hr><center><i><font color='red' size='3' font face='Italic'></font></i></center>
+<i>
+<font face="Courier New" color="BABABA" size="4"> #FuckGoverment <font face="Courier New" color="red" size="4">#FuckPolice<font face="Courier New" color="BABABA" size="4"> #HackedByAkashv<font face="Courier New" color="red" size="4"> #Sadness</font></font>
+
+
+</body>
+</html>
